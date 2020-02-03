0c5f431fc532d40b24349a2e0aeeae00e238a91b
Alex Morega <alex@grep.ro>
2020-02-03 16:25:27 +0200

    Return data as json over http

--- explore.js
+++ explore.js
@@ -1,22 +1,35 @@
+require('dotenv').config()
+const express = require('express')
 const Influx = require('influx')
 
 const influx = new Influx.InfluxDB({
-  host: 'influxdb',
+  host: process.env.INFLUX_HOST || 'influxdb',
+  port: process.env.INFLUX_PORT || '8086',
   database: 'linksintegrity'
 })
 
-async function main() {
-  try {
-    const result = await influx.query(`SELECT mean("value") AS "value" FROM "linksintegrity" WHERE time >= now() - 1d GROUP BY time(1d), "url" fill(none) ORDER BY time DESC LIMIT 1`)
-    const data = {}
-    for(const row of result) {
-      const {url, value} = row
-      data[url] = value
-    }
-    console.log(data)
-  } catch(e) {
-    console.error(e)
+async function query() {
+  const result = await influx.query(`SELECT mean("value") AS "value" FROM "linksintegrity" WHERE time >= now() - 1d GROUP BY time(1d), "url" fill(none) ORDER BY time DESC LIMIT 1`)
+  const data = {}
+  for(const row of result) {
+    const {url, value} = row
+    data[url] = value
   }
+  return data
 }
 
-main()
+const app = express()
+const port = process.env.PORT || '3000'
+
+app.get('/data', async (req, res) => {
+  try {
+    const data = await query()
+    res.json(data)
+  } catch(e) {
+    console.error(e)
+    res.status(500)
+    res.send("Internal error")
+  }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
