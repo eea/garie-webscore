7bcb93d8a4a01c8d404a093c52008aa0b36de035
Alex Morega <alex@grep.ro>
2020-02-26 16:41:59 +0200

    Launch a scan, wait for results, display results

--- src/ondemand.js
+++ src/ondemand.js
@@ -1,7 +1,34 @@
+const axios = require('axios')
+
 const jobs = {}
 
+const scan_ssllabs = async (url) => {
+  const res = await axios.post('http://garie-ssllabs:3000/scan', { url })
+  const { id } = res.data
+  const parseResult = (result) => {
+    return result.ssl_score
+  }
+  const poll = async () => {
+    const res = await axios.get(`http://garie-ssllabs:3000/scan/${id}`)
+    const { state, result } = res.data
+    if (state === 'success') {
+      scan.finished = true
+      scan.success = true
+      scan.result = parseResult(result)
+    }
+    if (state === 'error') {
+      scan.finished = true
+      scan.success = false
+    }
+    return scan
+  }
+  const scan = { name: "ssllabs", poll }
+  return scan
+}
+
 const launch = async (url) => {
-  const job = { url, id: new Date().getTime() }
+  const job = { url, id: new Date().getTime(), scans: [] }
+  job.scans.push(await scan_ssllabs(url))
   jobs[job.id] = job
   return job
 }
@@ -9,6 +36,12 @@ const launch = async (url) => {
 const poll = async (jobId) => {
   const job = jobs[jobId]
   if (!job) return null
+
+  for (const scan of job.scans) {
+    if (!scan.finished) {
+      await scan.poll()
+    }
+  }
 
   return job
 }
